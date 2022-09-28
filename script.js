d43701d9a9fb868b0096546b655f4f469ccd7386
Michael Walker <mikewalk517@gmail.com>
2022-09-28 10:05:55 -0400

    hw done woot

---
+++ script.js
@@ -0,0 +1,32 @@
+// const
+
+let num = 20;
+
+num = 15;
+
+num += 7;
+// num = num + 7;
+// console.log(num);
+
+num--;
+// console.log(num)
+
+num="This is not a number";
+
+let blank;
+
+console.log(blank);
+
+blank=null;
+console.log(blank);
+
+// BONUS
+const quarter=0.25, dime=0.10, nickel=0.05, penny=0.01;
+
+// const [quarter, dime, nickel, penny]=[0.25, 0.10, 0.05, 0.01];
+
+console.log(quarter, dime, nickel, penny);
+
+let letters="qwertyuiop";
+
+console.log(letters[letters.length-1]);
